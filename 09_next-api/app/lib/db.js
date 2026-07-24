4cee4b36a2147da61f7297b63d3b04d5f7556a58
M Rahman <51116745+rahman4ktr@users.noreply.github.com>
2026-07-24 12:04:00 +0000

    cached the mongoose

--- 09_next-api/app/lib/db.js
+++ 09_next-api/app/lib/db.js
@@ -1,11 +1,31 @@
 import mongoose from "mongoose";
 const MONGO_URI = process.env.MONGO_URI;
-// console.log(MONGO_URI);
-let connection = false;
-export default async function connectDB() {
-    const conn = mongoose.connect(MONGO_URI);
-    if(conn){
-        console.log("DB is connected");
-        connection = true;
-    }
+
+let cached = global.mongoose;
+if(!cached){
+    cached = global.mongoose = {conn:null, promise:null}
 }
+
+export default async function connectDB() {
+    if (cached.conn) return cached.conn;
+    if(!cached.conn){
+        cached.promise = mongoose.connect(MONGO_URI).then(()=>{
+            console.log("DB is connected!")
+        })
+    }
+    cached.conn = await cached.promise;
+    return cached.conn;    
+}
+
+//old code
+// import mongoose from "mongoose";
+// const MONGO_URI = process.env.MONGO_URI;
+// // console.log(MONGO_URI);
+// let connection = false;
+// export default async function connectDB() {
+//     const conn = mongoose.connect(MONGO_URI);
+//     if(conn){
+//         console.log("DB is connected");
+//         connection = true;
+//     }
+// }
